7a7a1a3b4fd81dbb78329b9267770d5719591016
roysta <roy@stapleton.biz>
2024-04-07 00:08:53 +0100

    Added primewire script.  Adds DVD quality button to form

---
+++ gm-primewire.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name        quality=DVD hidden form element - primewire.tf
+// @namespace   Violentmonkey Scripts
+// @match       https://www.primewire.tf/*
+// @grant       none
+// @version     1.0.1
+// @author      -
+// @description 25/11/2023, 23:57:16
+// ==/UserScript==
+
+var myform = document.getElementById('filter-bar');
+if (myform) {
+  const div = document.createElement("div");
+  const checked = t => (t.has('quality') && t.get('quality') === 'DVD')
+    ? "checked"
+    : ""
+  div.innerHTML=`<label for='quality'>DVD</label><input type='checkbox' id='quality' name='quality' value='DVD' ${checked(new URLSearchParams(window.location.search))}>`;
+  myform.appendChild(div);
+}
