506b56e4ea623e5d3c4d22ee27eaa728ef682d29
roysta <roy@stapleton.biz>
2024-04-07 00:54:09 +0100

    Added primewire remove checked.  Renamed scripts

---
+++ gm-primewire-remove-previous.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        primewire.tf remove 'Mark Watched' checkbox because it is in the way of the next episode button.
+// @namespace   Violentmonkey Scripts
+// @match       https://www.primewire.tf/*
+// @grant       none
+// @version     1.0.1
+// @author      roysta
+// @description 21/02/2024, 22:39:25
+// ==/UserScript==
+const id = document.getElementById('check-prev-episodes');
+id.parentElement.removeChild(id);
+
