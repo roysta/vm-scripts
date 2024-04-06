50bee9491ff9dd0224cf221e5e2710c09fee598c
roysta <roy@stapleton.biz>
2024-04-06 22:49:00 +0100

    Testing

--- gm-srsone.user.js
+++ gm-srsone.user.js
@@ -12,16 +12,16 @@ const uriArr = document.documentURI.split('/');
 let item = Number(uriArr[uriArr.length -1]);
 
 item = isNaN(item) || item < 11  ? 11  : item + 1;
-//console.log(`item: ${item}`);
+console.log(`item: ${item}`);
 
 const href = document.documentURI.replace(/(\/\d+)?$/,"/"+item);
-//console.log(`href: ${href}`);
+console.log(`href: ${href}`);
 
 const li1 = document.createElement('li');
 const li2 = document.createElement('li');
 
 li1.innerHTML = `<a href="${href}">${item}</a>`;
-//console.log(`li1.innerHTML: ${li1.innerHTML}`);
+console.log(`li1.innerHTML: ${li1.innerHTML}`);
 li2.innerHTML = `<a href="${href}">${item}</a>`;
 
 document.getElementsByClassName('spn-numbers')[0].childNodes[1].append(li1);
