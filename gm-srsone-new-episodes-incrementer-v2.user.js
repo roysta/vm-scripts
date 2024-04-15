9b80910997073c145d51cb57ccaa4eb4225f62ae
roysta <roy@stapleton.biz>
2024-04-15 19:11:34 +0100

    commented out debug logging

--- gm-srsone-new-episodes-incrementer-v2.user.js
+++ gm-srsone-new-episodes-incrementer-v2.user.js
@@ -15,10 +15,10 @@ let item = Number(uriArr[uriArr.length -1]);
 item = isNaN(item) 
     ? 1
     : item;
-console.log(`item:\t${item}`);
+//console.log(`item:\t${item}`);
 
 const href = document.documentURI.replace(/(\/\d+)?$/,"");
-console.log(`href:\t${href}`)
+//console.log(`href:\t${href}`)
 let i1 = document.createElement('ul');
 let i2 = document.createElement('ul');
 
@@ -27,7 +27,7 @@ for (let i = (item > 6 ? item - 5 : 1); i < (item > 6 ? item + 5 : 11); i+=1) {
     const li2 = document.createElement('li');
     li1.innerHTML = `<a ${item === i ? "class=\"current\"":""} href="${href}/${i}">${i}</a>`;
     li2.innerHTML = `<a ${item === i ? "class=\"current\"":""} href="${href}/${i}">${i}</a>`;
-    console.log(`i1:\t${i1.innerHTML}`)
+//    console.log(`i1:\t${i1.innerHTML}`)
     i1.append(li1);
     i2.append(li2);
 }
