fff5fb055a7bc7346061392843d922a3d97c268d
roysta <roy@stapleton.biz>
2024-04-15 18:56:17 +0100

    Added v2 script for episodes incrementor that re-writes the nav bar with item +- 5, and adds the current class.

---
+++ gm-srsone-new-episodes-incrementer-v2.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name        srsone new episodes incrementer v2
+// @namespace   Violentmonkey Scripts
+// @match       https://srsone.wf/new-shows
+// @include     https://srsone.wf/new-shows/*
+// @grant       none
+// @version     1.1.0
+// @author      roysta
+// @description 15/04/2024, 20:39:29
+// ==/UserScript==
+
+const uriArr = document.documentURI.split('/');
+let item = Number(uriArr[uriArr.length -1]);
+
+item = isNaN(item) 
+    ? 1
+    : item;
+
+const href = document.documentURI.replace(/(\/\d+)?$/,"");
+
+let i1 = document.createElement('ul');
+let i2 = document.createElement('ul');
+
+for (let i = (item > 6 ? item - 5 : 1); i < (item > 6 ? item + 5 : 11); i+=1) {
+    const li1 = document.createElement('li');
+    const li2 = document.createElement('li');
+    li1.innerHTML = `<a ${item === i ? "class=\"current\"":""} href="${href}/${$i}">${i}</a>`;
+    li2.innerHTML = `<a ${item === i ? "class=\"current\"":""} href="${href}/${$i}">${i}</a>`;
+    i1.append(li1);
+    i2.append(li2);
+}
+
+document.getElementsByClassName('spn-numbers')[0].childNodes[1].replaceWith(i1);
+document.getElementsByClassName('navigation')[1].childNodes[1].replaceWith(i2);
